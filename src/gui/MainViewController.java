c92643d245c60226adf7f87fabe5bc9867a71700
CLAUDISNEI <claudisneibello@gmail.com>
2020-09-13 13:25:17 -0300

    Atualizando o metodo AtualizarTela criando parâmetros do tipo funcao

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -3,6 +3,7 @@ package gui;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.Consumer;
 
 import application.Main;
 import gui.util.Alerts;
@@ -31,15 +32,22 @@ public class MainViewController implements Initializable {
 		System.out.println("Vendedor");
 	}
 
+	/*
+	 * Adicionado um parâmetro na função para carregar os dados na tableview
+	 */
 	@FXML
 	public void onItemMenuDepartamentoAction() {
-		carregaTela2("/gui/ListaDepartamento.fxml");
+		carregaTela("/gui/ListaDepartamento.fxml", (ListaDepartamentoControler controle) -> {
+			controle.setarDepartamentoServico(new DepartamentoServicos());
+			controle.atualizarTableView();
+		});
 	}
 
 	@FXML
 	public void onItemMenuAbaoutAction() {
 		// carrega o método para chamar a tela de about
-		carregaTela("/gui/About.fxml");
+		carregaTela("/gui/About.fxml", x -> {
+		});
 	}
 
 	@Override
@@ -47,45 +55,12 @@ public class MainViewController implements Initializable {
 
 	}
 
-	private synchronized void carregaTela(String nomeTela) {
-		try {
-			FXMLLoader loader = new FXMLLoader(getClass().getResource(nomeTela));
-
-			// janela que será aberta
-			VBox novoVBox = loader.load();
-
-			// pega a cena da tela princial main
-			Scene cenaPrincipal = Main.obterCenaPrincipal();
-
-			/*
-			 * variavel utilizada para converter o scrooplane da janela main em um vbox onde
-			 * será carregado o vbox da tela about
-			 */
-			VBox vboxTelaPrincipal = (VBox) ((ScrollPane) cenaPrincipal.getRoot()).getContent();
-
-			/*
-			 * variavel criada para guardar uma referência para o menu, preservando o menu
-			 * da tela principal
-			 */
-			Node menuPrincipal = vboxTelaPrincipal.getChildren().get(0);
-
-			// agora vamos limpar todos os filhos da tela principal
-			vboxTelaPrincipal.getChildren().clear();
-
-			/*
-			 * agora vamos adicionar os items que foram preservados nas variaveis acima na
-			 * tela principal, sendo que o novoVbox irá carregar a tela que forneceremos
-			 * como parâmetro do método
-			 */
-			vboxTelaPrincipal.getChildren().add(menuPrincipal);
-			vboxTelaPrincipal.getChildren().addAll(novoVBox.getChildren());
-
-		} catch (IOException e) {
-			Alerts.showAlerts("IOException", "Erro ao carregar a pagina", e.getMessage(), AlertType.ERROR);
-		}
-	}
-
-	private synchronized void carregaTela2(String nomeTela) {
+	/*
+	 * Agora a função é do tipo generica que possui um parâmetro que será passado
+	 * como o tipo de expressão lambda ou seja alem de carregar a janela, será
+	 * carregado também uma ação de atualizar a table view
+	 */
+	private synchronized <T> void carregaTela(String nomeTela, Consumer<T> inicializandoAcao) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource(nomeTela));
 
@@ -119,17 +94,17 @@ public class MainViewController implements Initializable {
 			vboxTelaPrincipal.getChildren().addAll(novoVBox.getChildren());
 
 			/*
-			 * Criado uma variavel da ListaDepartamenoControler para carregar os metodos na
-			 * inicialização da tela
+			 * 
+			 * retornando o controlador do tipo que foi atribuido na função
 			 */
-			ListaDepartamentoControler controles = loader.getController();
+			T controlador = loader.getController();
+
 			/*
-			 * cria uma instancia do DepartamentoServicos para carregar os dados da lista
-			 * que foi criada na classe DepartamentoServicos
+			 * função utilizada executar a ação, essa variavel foi declarada como parâmetro
+			 * na metodo
 			 */
-			controles.setarDepartamentoServico(new DepartamentoServicos());
-			// agora é possivel acessar o metodo atualizar tabela.
-			controles.atualizarTableView();
+
+			inicializandoAcao.accept(controlador);
 
 		} catch (IOException e) {
 			Alerts.showAlerts("IOException", "Erro ao carregar a pagina", e.getMessage(), AlertType.ERROR);
